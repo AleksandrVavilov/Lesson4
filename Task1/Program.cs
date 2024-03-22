42d4694a767213ea1886f9562ba630cef3691662
AleksandrVavilov <faqyou72@gmail.com>
2024-03-22 13:32:44 +0500

    l4

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] CreateMatrixRnd (int rowCount, int columsCount, int min, int max)
+{
+int[,] matrix = new int[rowCount,columsCount];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       matrix[i,j] = rnd.Next(min,max);
+    }
+}
+return matrix;
+}
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+int[,] matrix = CreateMatrixRnd(5, 5, 1, 100);
+ShowMatrix(matrix);
